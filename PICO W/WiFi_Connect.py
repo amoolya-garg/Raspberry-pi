297136b41b036075dca64fbe9274fc0ce961e655
amoolya-garg <amoolyagarg2001@gmail.com>
2023-08-16 12:18:12 +0530

    Create WiFi_Connect.py

---
+++ PICO W/WiFi_Connect.py
@@ -0,0 +1,21 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor,pico_led
+import machine
+
+ssid = "70906"
+password = "0003062001"
+
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid,password)
+    while wlan.isconnected() == False:
+            print("Waiting for connection...")
+            sleep(1)
+    try:
+        connect()
+    except KeyboardInterrupt:
+        machine.reset()
+        
